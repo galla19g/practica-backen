351c2b2d1300337ef28a9b05ab3b2dbc29881684
galla19g <magruesog22@itp.edu.co>
2026-06-10 20:15:00 -0500

    create

--- practica-backend-main/src/auth/dto/login-response.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class LoginResponseDto {
-  @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' })
-  access_token: string;
-}
--- practica-backend-main/src/common/common.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { RolesGuard } from './guards/roles.guard';
-
-@Global()
-@Module({
-  providers: [RolesGuard],
-  exports: [RolesGuard],
-})
-export class CommonModule {}
